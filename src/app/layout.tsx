937efb113ece2acea3f3e1f712f4b603d5d98da2
aminur-islam-sojib <sojibahmed.connect@gmail.com>
2026-08-04 20:22:40 +0600

    refactor: move public layout structure to a dedicated component file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Geist_Mono, Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "./globals.css";
 
-import AsideBar from "@/components/Shared/Asidebar/Asidebar";
-import Navbar from "@/components/Shared/Navbar/Navbar";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
@@ -137,25 +135,7 @@ export default function RootLayout({
         className={`${geistMono.variable} antialiased bg-[#121212]`}
         suppressHydrationWarning
       >
-        <section className="pb-10 max-w-7xl mx-auto">
-          <section className="grid grid-cols-4 py-5 pb-12 md:pb-0 sm:py-12 px-5 sm:px-5 md:px-10 gap-5 sm:gap-10">
-            <aside className="col-span-4 lg:col-span-1">
-              <div className="sticky top-12 overflow-y-auto">
-                <AsideBar />
-              </div>
-            </aside>
-
-            <main className="col-span-4 md:col-span-4 lg:col-span-3">
-              <div className="bg-[#1e1e1f] w-full border border-[#383838] rounded-2xl p-5 relative">
-                <div className="fixed bottom-0 left-0 right-0 z-50 lg:absolute lg:top-0 lg:right-0 lg:left-auto lg:bottom-auto">
-                  <Navbar />
-                </div>
-
-                <div className="w-full">{children}</div>
-              </div>
-            </main>
-          </section>
-        </section>
+        {children}
         <Analytics />
         <SpeedInsights />
       </body>
